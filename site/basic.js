ff0ab80cda46235fbcf02ea55ecc5df0bdf6d259
nenafay <nena.park@gmail.com>
2020-04-24 19:45:19 -0400

    updated mobile to fit design changes. Header is still a wreck.

--- site/basic.js
+++ site/basic.js
@@ -1,15 +1,15 @@
 function showNav() {
-    document.getElementById("myNavBox").classList.toggle("show")
+    document.getElementById("myNav").classList.toggle("show")
     ;
 }
 
 //Close the dropdown menu if the user clicks outside of it//
 window.onclick = function(event) {
     if (!event.target.matches('.navBtn')) {
-        var dropNav = document.getElementsByClassName("nav");
+        var dropDown = document.getElementsByClassName("nav");
         var i;
-        for (i=0; i < dropNav.length; i++) {
-            var openDropNav = dropNav[i];
+        for (i=0; i < myNav.length; i++) {
+            var openDropNav = dropDown[i];
             if (openDropNav.classList.contains('show')) {
                 openDropNav.classList.remove('show');
             }
